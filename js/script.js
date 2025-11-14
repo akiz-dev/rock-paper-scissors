fb3dd963d87418c5a68a004529bf3065d8e2f0ab
akiz <akiz.dev@gmail.com>
2025-11-14 17:07:19 -0300

    create getComputerChoice function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-
+// computer choice
+function getComputerChoice() {
+     const choices = ["rock", "paper", "scissors"];
+     const computerChoice = Math.floor(Math.random() * choices.length);
+     return choices[computerChoice];
+}
+console.log(getComputerChoice());
