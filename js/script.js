6d724b40a06eab493def4ce662e8ccd2c098fea6
akiz <akiz.dev@gmail.com>
2025-11-14 17:24:52 -0300

    Create getHumanChoice function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
-// computer choice
+// computer choice func
 function getComputerChoice() {
      const choices = ["rock", "paper", "scissors"];
      const computerChoice = Math.floor(Math.random() * choices.length);
      return choices[computerChoice];
 }
-console.log(getComputerChoice());
+console.log(getComputerChoice())
+
+// human choice func
+function getHumanChoice() {
+     const humanChoice = prompt("Type only one: 'Rock', 'Paper' or 'Scissors'");
+
+     return humanChoice;
+}
+console.log(getHumanChoice());
+// getHumanChoice();
